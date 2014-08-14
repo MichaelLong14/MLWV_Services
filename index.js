a565d3063e01104e3f77712735b5ed25e731fd8b
MichaelLong14 <long.t.michael@gmail.com>
2014-08-13 23:01:14 -0400

    updated formatting

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ var Joi = require("joi");
 var Handlers = require("./lib/handlers");
 
 var serverOptions = {
-  cors: {
-    additionalHeaders: ['X-Api-Key', 'Accept', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Api-Key'],
-    credentials: true
-  }
+	cors: {
+		additionalHeaders: ['X-Api-Key', 'Accept', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Api-Key'],
+		credentials: true
+	}
 };
 
 var port = process.env.port || 5000;
 var server = new Hapi.Server(port, serverOptions);
 
 var pack = require("package"),
-  swaggerOptions = {
-    basePath: 'http://localhost:5000',
-    apiVersion: pack.version
-  };
+	swaggerOptions = {
+		basePath: 'http://localhost:5000',
+		apiVersion: pack.version
+	};
 
-var routeArray = [
-	{
+var routeArray = [{
 		method: 'GET',
 		path: '/spots',
 		config: {
@@ -40,7 +39,7 @@ var routeArray = [
 			tags: ['api']
 		}
 	},
-	
+
 	{
 		method: 'DELETE',
 		path: '/spot/{id?}',
@@ -56,20 +55,20 @@ var routeArray = [
 server.route(routeArray);
 
 server.pack.register({
-  plugin: require('hapi-swagger'),
-  options: swaggerOptions
+	plugin: require('hapi-swagger'),
+	options: swaggerOptions
 }, function(err) {
-  if (err) {
-    server.log(['error'], 'Plugin "hapi-swagger" load error: ' + err);
-  } else {
-    server.log(['start'], 'Swagger interface loaded');
-    //Don't need to actually start the server to run Lab tests.
-    if (!module.parent) {
-      server.start(function() {
-        console.log(['start'], pack.name + ' - web interface: ' + server.info.uri);
-      });
-    }
-  }
+	if (err) {
+		server.log(['error'], 'Plugin "hapi-swagger" load error: ' + err);
+	} else {
+		server.log(['start'], 'Swagger interface loaded');
+		//Don't need to actually start the server to run Lab tests.
+		if (!module.parent) {
+			server.start(function() {
+				console.log(['start'], pack.name + ' - web interface: ' + server.info.uri);
+			});
+		}
+	}
 });
 
 module.exports = server;
